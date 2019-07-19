04069f6b583b6686d5f13c9d1a72521bab43bf6c
lvijudsky <contato@lvijudsky.com.br>
2019-07-19 09:32:13 -0300

    Adicionando filmes

---
+++ filme.php
@@ -0,0 +1,61 @@
+<?php
+	include('./includes/filmes.php');
+	include('./includes/generos.php');
+	$id = 1* $_GET['id'];
+	$f = $filmes[$id];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Turma da Mônica - Laços - Clube do Filme</title>
+
+	<!-- Meu estilo -->
+	<link rel="stylesheet" href="./assets/css/geral.css">
+	<link rel="stylesheet" href="./assets/css/filme.css">
+</head>
+
+<body>
+	<nav>
+		<ul>
+			<?php
+				foreach($generos as $g){
+					echo('<li><a href="#">'.$g.'</a></li>');
+				}
+			?>
+		</ul>
+		<form>
+			<input type="text" name="trecho"><button type="submit">Buscar</button>
+		</form>
+	</nav>
+	<main>
+
+		<h2>Turma da Mônica - Laços</h2>
+		<img src="./assets/img/cartazes/cartaz-<?= $id ?>.png" alt="Turma da Mônica - Laços" class="cartaz">
+		<section>
+
+			<div class="sinopse">
+				<h5>Sinopse</h5>
+				<div><?= $f['sinopse'] ?></div>
+			</div>
+
+			<div class="censura">
+				<h5>Censura</h5>
+				<div><?= ($f['censura'] == 0) ? ('Livre') : ($f['censura']) ?></div>
+			</div>
+			
+			<div class="critica">
+				<h5>Crítica</h5>
+				<div><?= $f['critica'] ?></div>
+			</div>
+		</section>
+
+		<?= $f['trailer'] ?>		
+	</main>
+</body>
+
+</html>
